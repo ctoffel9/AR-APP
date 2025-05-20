1d42bc3831c09c108a9bfee34418fd6fcad67e80
Chrsitoffel <71421415+ctoffel9@users.noreply.github.com>
2025-05-20 22:50:43 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/AR-APP.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/New folder (3).json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
